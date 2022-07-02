841f871de8c8ca1a8d833296936e99275db36bfc
auagho <auagho.o@gmail.com>
2022-07-02 23:32:30 +0900

    주석 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,32 @@
-// import
+// path: NodeJS에서 파일 및 디렉토리 경로 작업을 위한 전역 모듈
 const path = require("path");
 const HtmlPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
-// export
 module.exports = {
   // 파일을 읽어들이기 시작하는 진입점 설정
   entry: "./js/main.js",
 
   // 결과물(번들)을 반환하는 설정
   output: {
+    // 주석은 기본값!, `__dirname`은 현재 파일의 위치를 알려주는 NodeJS 전역 변수
     // path: path.resolve(__dirname, "dist"),
     // filename: "main.js",
     clean: true,
   },
 
+  // 모듈 처리 방식을 설정
   module: {
     rules: [
       {
         test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+        use: [
+          // 순서 중요!
+          "style-loader",
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
       },
       {
         test: /\.js$/,
